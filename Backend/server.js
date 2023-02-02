fad74a01d2c33dbae2c7f799f7d9f4d94cd3227c
Mary Kelly <mk-km@live.com>
2023-02-01 21:53:10 -0500

    modified checkout slightly (nothing deleted, just commented out) and included code that sends data back and forth between your front and backends

--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,27 @@
 // import Cart from "../Frontend/src/pages/Cart"
 
 // This is your test secret API key.
-const stripe = require('stripe')('sk_test_51MQg97Ipd7r3FnqnO5KhLULIzxDxRKcbJD9vNOfK9DZ8c4YJi7ISum1EV56rzJKpqC5qVBjyovezME2HSDchczab002fkVuyCf');
+const stripe = require("stripe")("sk_test_51MQg97Ipd7r3FnqnO5KhLULIzxDxRKcbJD9vNOfK9DZ8c4YJi7ISum1EV56rzJKpqC5qVBjyovezME2HSDchczab002fkVuyCf");
 
 // Sets Up Express server
-const express = require('express');
+const express = require("express");
 
 // Method To Handle CORS (Cross-Origin Resource Sharing) Headers
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 
-app.get('/', (req, res) => {
-    // res.send('hello world')
-    res.send('home')
-})
-app.get('/cart', function(req, res) {
-    res.send('cart test')
-})
+app.get("/", (req, res) => {
+	// res.send('hello world')
+	res.send("home");
+});
+app.get("/cart", function (req, res) {
+	res.send("cart test");
+});
 // Serve A Public Directory
-app.use(express.static('public'));
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // const ShowCart = require("../Frontend/src/pages/Cart");
 // const { default: Cart } = require('../Frontend/src/pages/Cart');
@@ -30,42 +30,49 @@ app.use(express.urlencoded({extended: true}))
 // const total = ShowCart('subtotal')
 // const quantity = ShowCart('totalItems')
 
-const YOUR_DOMAIN = 'http://localhost:3000';
+const YOUR_DOMAIN = "http://localhost:3000";
 
 // New Route On Express server For POST Request To Endpoint '/CheckoutFinal'
 // Runs Async Function That Creates Checkout Session On Stripe Platform
 
+app.post("/CheckoutFinal", async (req, res) => {
+	console.log("b");
 
-app.post('/CheckoutFinal', async (req, res) => {
-    console.log('b')
+	// Method To Create Checkout Session That Takes Object As Argument That Contains Information
+	const session = await stripe.checkout.sessions.create({
+		line_items: [
+			{
+				// Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+				// price: 'total',
+				// quantity: 'quantity',
+			},
+		],
+		mode: "payment",
+		success_url: `${YOUR_DOMAIN}?success=true`,
+		cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+		automatic_tax: { enabled: true },
+	});
 
-// Method To Create Checkout Session That Takes Object As Argument That Contains Information
-const session = await stripe.checkout.sessions.create({
-line_items: [
-    {
-    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-    // price: 'total',
-    // quantity: 'quantity',
-    }
-],
-mode: 'payment',
-success_url: `${YOUR_DOMAIN}?success=true`,
-cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-automatic_tax: {enabled: true},
-});
-
-// Checkout Session URL Extracted From Response Of Stripe API Then Sent Back To Client In Response Of Endpoint In JSON Format
-res.json({url: session.url})
-// res.redirect(303, session.url);
+	// Checkout Session URL Extracted From Response Of Stripe API Then Sent Back To Client In Response Of Endpoint In JSON Format
+	res.json({ url: session.url });
+	// res.redirect(303, session.url);
 });
 // require('./routes/cart.routes');
 
-const cartRoutes = require('./routes/cart.routes'); 
-app.use('/cart', cartRoutes);
-app.listen(3001, () => console.log('Running on port 3001'));
+app.post("/checkout", async (req, res) => {
+	// Data sent from frontend axios request in data param is accessed on req.body
+	console.log("REQUEST DATA: ", req.body);
+    // console.log should be visible in your server terminal
 
+	// you would likely want to handle stripe here and now have access to the data sent over
 
+	// send back successful status of 200 and whatever data we want to send back -- so an object with message in this case
+	res.status(200).json({ message: "Success" });
+});
 
+const cartRoutes = require("./routes/cart.routes");
+app.use("/cart", cartRoutes);
+app.listen(3001, () => console.log("Running on port 3001"));
 
 // something.routes.js
 // module.exports = (app) => {
@@ -79,10 +86,3 @@ app.listen(3001, () => console.log('Running on port 3001'));
 //     .then(Product => res.json(cart))
 //     .catch(err => res.json(err))
 // }
-
-
-
-
-
-
-
--- Frontend/src/pages/Checkout.jsx
+++ Frontend/src/pages/Checkout.jsx
@@ -10,139 +10,201 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 // Payment Option's Image's
-import Credit from "../components/Credit"
+import Credit from "../components/Credit";
 
-import ProductDisplay from '../payment/app.jsx'
+import ProductDisplay from "../payment/app.jsx";
 
 // AOS Effects
-import { useEffect} from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import axios from "axios";
 
 const Checkout = () => {
+	// Check Current Cart State
+	const state = useSelector((state) => state.handleCart);
 
-  // Check Current Cart State
-  const state = useSelector((state) => state.handleCart);
-  
-  // AOS Effects
-  useEffect(() => {
-    AOS.init();
-  }, [])
+	// AOS Effects
+	useEffect(() => {
+		AOS.init();
+	}, []);
 
-  // No Item Cart
-  const EmptyCart = () => {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 py-5 bg-light text-center">
-            <h4 className="p-3 display-5">No item in Cart</h4>
+	// No Item Cart
+	const EmptyCart = () => {
+		return (
+			<div className='container'>
+				<div className='row'>
+					<div className='col-md-12 py-5 bg-light text-center'>
+						<h4 className='p-3 display-5'>No item in Cart</h4>
 
-            {/* Link To Prodcuts */}
-            <Link to="/" className="btn btn-outline-dark mx-4">
-              <i className="fa fa-arrow-left"></i> Continue Shopping
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  };
+						{/* Link To Prodcuts */}
+						<Link to='/' className='btn btn-outline-dark mx-4'>
+							<i className='fa fa-arrow-left'></i> Continue Shopping
+						</Link>
+					</div>
+				</div>
+			</div>
+		);
+	};
 
-  const ShowCheckout = () => {
+	const ShowCheckout = () => {
+		// If Empty Cart Then EmptyCart Function
+		if (!state.length) {
+			return <EmptyCart />;
+		}
 
-    // If Empty Cart Then EmptyCart Function
-    if(!state.length){
-      return <EmptyCart/>
-    }
+		// Cart Total
+		let subtotal = 0;
+		let shipping = 30.0;
+		let totalItems = 0;
 
-    // Cart Total
-    let subtotal = 0;
-    let shipping = 30.0;
-    let totalItems = 0;
+		state.forEach((item) => {
+			subtotal += item.price * item.qty;
+			totalItems += item.qty;
+		});
 
-    state.forEach((item) => {
-      subtotal += item.price * item.qty;
-      totalItems += item.qty;
-    });
+		return (
+			<>
+				{/* Cart Body */}
+				<div className='container py-5'>
+					<div className='row my-4'>
+						{/* Right Side Cart */}
+						<div className='col-md-5 col-lg-4 order-md-last'>
+							<div className='card mb-4'>
+								{/* Header */}
+								<div className='card-header py-3 bg-light'>
+									<h5 className='mb-0'>Order Summary</h5>
+								</div>
+								<table className='table mt-3 mb-3'>
+									<tbody>
+										{/* Quantity and Price */}
+										<tr>
+											<td>Products ({totalItems})</td>
+											<td>${Math.round(subtotal)}</td>
+										</tr>
 
-    
-    return (
-      <>
-        {/* Cart Body */}
-        <div className="container py-5">
-          <div className="row my-4">
+										{/* Shipping Price */}
+										<tr>
+											<td>Shipping</td>
+											<td>${shipping}</td>
+										</tr>
 
-            {/* Right Side Cart */}
-            <div className="col-md-5 col-lg-4 order-md-last">
-              <div className="card mb-4">
+										{/* Total Amount */}
+										<tr>
+											<td>
+												<strong>Total amount</strong>
+											</td>
+											<td>
+												<strong>${Math.round(subtotal + shipping)}</strong>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+							{/*  This button will handle sending back logic to your backend on click */}
+							<button
+								className='btn btn-dark btn-lg btn-block'
+								onClick={async () => {
+									const products = state;
 
-                {/* Header */}
-                <div className="card-header py-3 bg-light">
-                  <h5 className="mb-0">Order Summary</h5>
-                </div>
-                <table className="table mt-3 mb-3">
-                  <tbody>
+									let response = await axios({
+										method: "post",
+										url: "http://localhost:3001/checkout",
+										data: {
+											products,
+										},
+									});
 
-                    {/* Quantity and Price */}
-                    <tr>
-                      <td>Products ({totalItems})</td>
-                      <td>${Math.round(subtotal)}</td>
-                    </tr>
+									// Should see the response we sent back from the server in the browser console
+									console.log("RESPONSE FROM SERVER: ", response);
+								}}>
+								Confirm Checkout
+							</button>
+						</div>
 
-                    {/* Shipping Price */}
-                    <tr>
-                      <td>Shipping</td>
-                      <td>${shipping}</td>
-                    </tr>
+						{/* Left Side Cart */}
+						<div className='col-md-7 col-lg-8'>
+							<div className='card mb-4'>
+								{/* Header */}
+								<div className='card-header py-3'>
+									<h4 className='mb-0'>Total Summary</h4>
+								</div>
 
-                    {/* Total Amount */}
-                    <tr>
-                      <td><strong>Total amount</strong></td>
-                      <td><strong>${Math.round(subtotal + shipping)}</strong></td>
-                    </tr>
-                  </tbody>
-              </table>
-              </div>
-            </div>
+								{/* Stripe */}
+								{/* <ProductDisplay /> */}
 
-            {/* Left Side Cart */}
-            <div className="col-md-7 col-lg-8">
-              <div className="card mb-4">
-                {/* Header */}
-                <div className="card-header py-3">
-                  <h4 className="mb-0">Total Summary</h4>
-                </div>
+								{/* Copied your cart screen and removed users editing ability so they must confirm checkout */}
+								<div className='card-body'>
+									{state.map((item) => {
+										return (
+											// Product ID
+											<div key={item.id}>
+												<div className='row d-flex align-items-center'>
+													{/* Product Image */}
+													<div className='col-lg-3 col-md-12'>
+														<div className='bg-image rounded' data-mdb-ripple-color='light'>
+															{/* Image Link To Product */}
+															<Link to={"/product/" + item.id}>
+																{/* Image Hover Effect */}
+																<div className='box w-content'>
+																	<img src={item.image} alt={item.title} width={100} height={75} />
+																</div>
+															</Link>
+														</div>
+													</div>
 
-                {/* Stripe */}
-                <ProductDisplay />
+													{/* Product Title */}
+													<div className='col-lg-5 col-md-6 graphic3'>
+														{/* Text Link To Product */}
+														<h4>
+															<strong>{item.title}</strong>
+														</h4>
+													</div>
 
-                {/* Payment Options */}
-                <Credit />
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-  return (
-    <>
-      {/* Navbar */}
-      <Navbar />
-      <div className="container my-3 py-3">
+													{/* Quantity Change and Price */}
+													<div className='col-lg-4 col-md-6'>
+														{/* Quantity and Price */}
+														<p className='text-start text-md-center graphic3'>
+															<strong>
+																<span className='text-muted'>{item.qty}</span> x ${item.price}
+															</strong>
+														</p>
+													</div>
+												</div>
+												<hr className='my-4' />
+											</div>
+										);
+									})}
+								</div>
 
-        {/* Header */}
-        <h1 className="text-center improv" data-aos="flip-up">Checkout</h1>
-        <hr />
+								{/* Payment Options */}
+								<Credit />
+							</div>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	};
+	return (
+		<>
+			{/* Navbar */}
+			<Navbar />
+			<div className='container my-3 py-3'>
+				{/* Header */}
+				<h1 className='text-center improv' data-aos='flip-up'>
+					Checkout
+				</h1>
+				<hr />
 
-        {/* Empty or Full Cart? */}
-        {state.length ? <ShowCheckout /> : <EmptyCart />}
-      </div>
+				{/* Empty or Full Cart? */}
+				{state.length ? <ShowCheckout /> : <EmptyCart />}
+			</div>
 
-      {/* Footer */}
-      <Footer />
-    </>
-  );
+			{/* Footer */}
+			<Footer />
+		</>
+	);
 };
 
 export default Checkout;
